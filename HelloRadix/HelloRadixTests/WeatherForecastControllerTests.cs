dd311a16aaabfa2798e90136b98aca110bcd88b8
Szidoniaa <szidonia.antal@gmail.com>
2021-03-19 11:33:32 +0100

    sln and dockerfile in place

---
+++ HelloRadix/HelloRadixTests/WeatherForecastControllerTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using HelloRadix;
+using HelloRadix.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace HelloRadixTests
+{
+    public class WeatherForecastControllerTests
+    {
+        private Mock<ILogger<WeatherForecastController>> _loggerMock;
+        private WeatherForecastController _underTest;
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            _underTest = new WeatherForecastController(_loggerMock.Object);
+        }
+
+        [Test]
+        public void Get_Should_Return_Expected_DataType()
+        {
+            var result = _underTest.Get();
+            Assert.IsInstanceOf<IEnumerable<WeatherForecast>>(result);
+        }
+
+        //[Test]
+        //public void WantThisToFail()
+        //{
+        //    Assert.IsTrue(false);
+        //}
+    }
+}
